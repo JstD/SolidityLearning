89416ada34e0b900753b89dbb8fa52448ec1550e
jstD <dung.truong2000@hcmut.edu.vn>
2021-05-14 13:48:29 +0700

    update my progress

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -3,7 +3,7 @@ const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-  "juice bicycle seek common shield hello below angry source share exact mobile",
+  "churn trick snake riot antenna leader sudden border bread monster together mandate",
   // remember to change this to your own phrase!
   "https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c"
   // remember to change this to your own endpoint!
